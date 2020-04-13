1a82a09973276fc6651fb706eabf52cef73d504e
Nana Kwame Owusu <s.owusu45@gmail.com>
2020-04-13 18:31:19 -0500

    encrypt working

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Encrypted-G-Drive
 Project to encrypt files stored in Google Drive
+
+
+## Setting up Virtru credentials
+
+Once `credentials.json` file has been created by google drive api, update it as below with virtru app id and email obtained from Virtru dashboard:
+
+```
+{
+    "installed": {...},
+    "virtru": {
+        "app_id": "50395bc5-1dc0-4b6f-9d36-ccf6c14df395",
+        "email": "s.owusu45@gmail.com"
+    }
+}
+```
--- upload node.js
+++ upload node.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
+const Virtru = require('virtru-sdk');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
@@ -24,14 +25,15 @@ fs.readFile('credentials.json', (err, content) => {
  */
 function authorize(credentials, callback) {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const virtru_app = credentials.virtru;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
+    if (err) return getAccessToken(oAuth2Client, callback, virtru_app);
     oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
+    callback(oAuth2Client, virtru_app);
   });
 }
 
@@ -64,7 +66,7 @@ function listFiles(auth) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getAccessToken(oAuth2Client, callback) {
+function getAccessToken(oAuth2Client, callback, virtru_app) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES,
@@ -84,33 +86,61 @@ function getAccessToken(oAuth2Client, callback) {
         if (err) return console.error(err);
         console.log('Token stored to', TOKEN_PATH);
       });
-      callback(oAuth2Client);
+      callback(oAuth2Client, virtru_app);
     });
   });
 }
 
+async function encrypt(filename, virtru_app) {
+  const email = virtru_app.email;
+  const appId = virtru_app.app_id;
+  console.log(email, appId);
+  // Initialize the client.
+  const client = new Virtru.Client({email, appId});
+  // For file, encrypt using the helper function.
+  const encryptParams = new Virtru.EncryptParamsBuilder()
+    .withFileSource(filename)
+    .build();
+  ct = await client.encrypt(encryptParams);
+  // Return the file write completion promise.
+  return ct.toFile(`${filename}.tdf3.html`);
+}
 
-function uploadFile(auth){
-  var folderId = '1AxGsisAfHSYeZQ8-rO5Ybf35aEpe_ouE';
-  const drive = google.drive({version: 'v3', auth});
-  var fileMetadata = {
-  'name': 'cat.jpg',
-  parents: [folderId]
-};
-var media = {
-  mimeType: 'image/jpeg',
-  body: fs.createReadStream('cat.jpg')
-};
-drive.files.create({
-  resource: fileMetadata,
-  media: media,
-  fields: 'id'
-}, function (err, file) {
-  if (err) {
-    // Handle error
-    console.log(err);
-  } else {
-    console.log('File Id: ', file.data.id);
-  }
-});
+
+function uploadFile(auth, virtru_app){
+  const fileToEncryptName = "cat.jpg";
+
+  const promise = encrypt(fileToEncryptName, virtru_app);
+  promise.then(() => 
+  {
+    console.log(`File, ${fileToEncryptName} has been encrypted and written to ${fileToEncryptName}.tdf3.html!`)
+
+    //var folderId = '1AxGsisAfHSYeZQ8-rO5Ybf35aEpe_ouE';
+    const drive = google.drive({version: 'v3', auth});
+    const encrypteFileName = `${fileToEncryptName}.tdf3.html`;
+    var fileMetadata = {
+    'name': encrypteFileName
+    //,parents: [folderId]
+  };
+  var media = {
+    mimeType: 'image/jpeg',
+    body: fs.createReadStream(encrypteFileName)
+  };
+  drive.files.create({
+    resource: fileMetadata,
+    media: media,
+    fields: 'id'
+  }, function (err, file) {
+    if (err) {
+      // Handle error
+      console.log(err);
+    } else {
+      console.log('File Id: ', file.data.id);
+    }
+  });
+
+
+  });
+
+  
 }
